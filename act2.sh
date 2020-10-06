ff5acea75a359f36a05039d4204f74a9b4a72ff7
MICHAEL <MJENNINGS6@gmail.com>
2020-10-05 20:08:03 -0400

    added princess leia file and text, and add obi wan file

--- act2.sh
+++ act2.sh
@@ -1,7 +1,7 @@
 # 1. Inside the `star_wars/rebellion` directory, [IN ONE COMMAND!!] create a file called `princess_leia` with the text `Help me, Obi-Wan…You’re my only hope.`
-
+echo "Help me, Obi-Wan…You’re my only hope." > star_wars/rebellion/princess_leia
 # 2. Create a file called obi_wan in star_wars/rebellion.
-
+touch star_wars/rebellion/obi_wan
 # 3. Create a file in star_wars/rebellion called luke_skywalker.
 
 # 4. Create a *directory* in star_wars/rebellion called millenium_falcon.
