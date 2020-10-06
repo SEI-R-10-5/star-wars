469a815e6745cc8c2b5c31296200de2677234e8c
dmalkemes <dmalkemes@gmail.com>
2020-10-05 19:06:07 -0500

    more better answers act 1

--- act1.sh
+++ act1.sh
@@ -5,12 +5,12 @@ mkdir star_wars/empire
 mkdir star_wars/rebellion
 # 3. Inside the empire directory, create a file called darth_vader.
 cd stars_wars/empire
-touch darth_vader.txt
+touch darth_vader
 # 4. Use the force (or your echo) to add the text ...heavy breathing... to the darth_vader file. (Don’t remember how to do this? Use the ‘other force’, known as Google!)
-echo "...heavy breathing..." > darth_vader.txt 
+echo "...heavy breathing..." >> darth_vader
 # 5. Inside the empire directory, create a file called emperor_palpatine.
-touch emperor_palpatine.txt
+touch emperor_palpatine
 # 6. Inside the empire directory, create a directory called death_star.
 mkdir death_star
 # 7. Move darth_vader into the death_star.
-mv darth_vader.txt ./death_star
+mv darth_vader ./death_star
