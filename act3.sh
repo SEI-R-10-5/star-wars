ed01200a51e5701932ab730ffc9456dbcca90ade
Mike Zahuta <mike.zahuta@gmail.com>
2020-10-06 09:14:41 -0400

    had to add the -r to the remove

--- act3.sh
+++ act3.sh
@@ -15,4 +15,4 @@ mv star_wars/empire/death_star/millenium_falcon star_wars/rebellion/
 # 5. darth_vader leaves the death_star to pursue Luke! Move him from the death_star into the empire directory!
 mv star_wars/empire/death_star/darth_vader star_wars/empire/
 # 6. Thanks to his practice back home at Beggar’s Canyon, Luke blew up the death_star! Remove it from the galaxy!
-rm star_wars/empire/death_star/
+rm -r star_wars/empire/death_star/
