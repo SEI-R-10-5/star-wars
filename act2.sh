2417b9778b6918046e5fe7537a749c15b4960346
MICHAEL <MJENNINGS6@gmail.com>
2020-10-05 20:26:12 -0400

    added skywalker file, made falcon dir, added hansolo an chewy file, mv skywalker, obi and leia to falcon

--- act2.sh
+++ act2.sh
@@ -3,13 +3,13 @@ echo "Help me, Obi-Wan…You’re my only hope." > star_wars/rebellion/princess_
 # 2. Create a file called obi_wan in star_wars/rebellion.
 touch star_wars/rebellion/obi_wan
 # 3. Create a file in star_wars/rebellion called luke_skywalker.
-
+touch star_wars/rebellion/luke_skywalker
 # 4. Create a *directory* in star_wars/rebellion called millenium_falcon.
-
+mkdir star_wars/rebellion/millenium_falcon
 # 5. In two commands, inside the millenium_falcon, create two files: han_solo and chewy.
-
+touch star_wars/rebellion/millenium_falcon/{han_solo,chewy}
 # 6. In THREE commands, move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
-
-
-
+mv star_wars/rebellion/luke_skywalker star_wars/rebellion/millenium_falcon
+mv star_wars/rebellion/obi_wan star_wars/rebellion/millenium_falcon
+mv star_wars/rebellion/princess_leia star_wars/rebellion/millenium_falcon
 # 7. Move the millenium_falcon into the death_star.
