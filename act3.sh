a93e59c369ed6d118caed1c8e9d4665406cfb5e9
Mike Zahuta <mike.zahuta@gmail.com>
2020-10-05 23:41:27 -0400

    maybe I forgot to move Chewy into the death_star

--- act3.sh
+++ act3.sh
@@ -9,7 +9,7 @@ rm star_wars/empire/death_star/obi_wan
 mv star_wars/empire/death_star/han_solo star_wars/empire/death_star/millenium_falcon/
 mv star_wars/empire/death_star/luke_skywalker star_wars/empire/death_star/millenium_falcon/
 mv star_wars/empire/death_star/princess_leia star_wars/empire/death_star/millenium_falcon/
-mv star_wars/empire/death_star/chewy star_wars/empire/death_star/millenium_falcon/
+mv star_wars/empire/chewy star_wars/empire/death_star/millenium_falcon/
 # 4. Move the millenium_falcon back into the rebellion directory.
 mv star_wars/empire/death_star/millenium_falcon star_wars/rebellion/
 # 5. darth_vader leaves the death_star to pursue Luke! Move him from the death_star into the empire directory!
