34446b7398a057a8ecb079b2ff6e31077cd624d1
lewist13 <lewistpro@gmail.com>
2020-10-05 23:00:04 -0400

    still fucked

--- act3.sh
+++ act3.sh
@@ -2,12 +2,15 @@
 # Move the whole crew from the millenium_falcon directory into the death_star directory.
 mv star_wars/empire/death_star/millenium_falcon/* star_wars/empire/death_star
 # 2. darth_vader has defeated obi_wan! Delete poor obi_wan.
-rm star_wars/empire/death_star/millenium_falcon/obi_wan
+rm star_wars/empire/death_star/obi_wan
 # 3.  Our heroes have disabled the tractor beam! In four commands, Move the whole crew back into the millenium_falcon!
 # Remember: darth_vader remains in the death_star and emperor_palpatine is still in the empire.
-
+mv star_wars/empire/death_star/hans_solo star_wars/empire/death_star/millenium_falcon
+mv star_wars/empire/death_star/chewy star_wars/empire/death_star/millenium_falcon
+mv star_wars/empire/death_star/princess_leia star_wars/empire/death_star/millenium_falcon
+mv star_wars/empire/death_star/luke_skywalker star_wars/empire/death_star/millenium_falcon
 # 4. Move the millenium_falcon back into the rebellion directory.
-
+mv star_wars/empire/death_star/millenium_falcon star_wars/rebellion/millenium_falcon
 # 5. darth_vader leaves the death_star to pursue Luke! Move him from the death_star into the empire directory!
-
+mv star_wars/empire/death_star/darth_vader star_wars/empire/darth_vader
 # 6. Thanks to his practice back home at Beggar’s Canyon, Luke blew up the death_star! Remove it from the galaxy!
