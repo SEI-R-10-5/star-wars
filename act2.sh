44ee0333ef245e31205ca8aa5db727e67e28f6ba
MICHAEL <MJENNINGS6@gmail.com>
2020-10-05 21:59:30 -0400

    correction to #5, did it in one command which was breaking the test

--- act2.sh
+++ act2.sh
@@ -7,7 +7,8 @@ touch star_wars/rebellion/luke_skywalker
 # 4. Create a *directory* in star_wars/rebellion called millenium_falcon.
 mkdir star_wars/rebellion/millenium_falcon
 # 5. In two commands, inside the millenium_falcon, create two files: han_solo and chewy.
-touch star_wars/rebellion/millenium_falcon/{han_solo,chewy}
+touch star_wars/rebellion/millenium_falcon/han_solo
+touch star_wars/rebellion/millenium_falcon/chewy
 # 6. In THREE commands, move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
 mv star_wars/rebellion/luke_skywalker star_wars/rebellion/millenium_falcon
 mv star_wars/rebellion/obi_wan star_wars/rebellion/millenium_falcon
